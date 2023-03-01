83b8b3595e3129f79bd1770c9e271966d5b1f432
Michael <c21502889@mytudubllin.ie>
2023-03-01 12:12:01 +0000

    commit2

--- endless-runner-2023/scenes/entities/enemies/firewall/player_dectection/decect_player_area/detect_player_area.gd
+++
@@ -1,9 +0,0 @@
-extends Spatial
-
-
-onready var _enemyDeathAnimation: Spatial = get_parent().get_node("EnemyDeathAnimation") 
-onready var _owner: KinematicBody = get_owner()
-
-func freeEnemy():
-	_enemyDeathAnimation.playDeathAnimation()
-	_owner.queue_free()
