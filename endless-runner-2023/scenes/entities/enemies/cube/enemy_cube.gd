26d37ae080c6d95abaad4624d4edf6ff1dc41c5b
Michael <c21502889@mytudubllin.ie>
2023-03-01 15:11:02 +0000

    pattern for firewall

--- endless-runner-2023/scenes/entities/enemies/cube/enemy_cube.gd
+++ endless-runner-2023/scenes/entities/enemies/cube/enemy_cube.gd
@@ -6,6 +6,13 @@ extends Spatial
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _enter_tree():
+	PlayerStates.firefall_spawn = true
 
+func _physics_process(delta):
+	if (PlayerStates.kill_firewall == true):
+		print("I'm dead D:")
+		PlayerStates.firefall_spawn = false
+		free()
+		
+		
---
+++ endless-runner-2023/scenes/entities/enemies/firewall/movement/movement.gd
@@ -0,0 +1,48 @@
+extends Spatial
+class_name EnemyCubeMovement
+
+
+# Private Variables
+var velocity: Vector3
+
+
+# Nodes
+onready var node_forward: Spatial = $Forward
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	velocity = Vector3.ZERO
+
+
+func _process(_delta):
+	if(node_forward == null):
+		return
+	freeEnemy()
+	node_forward.movementForward()
+
+
+
+# Setter Functions
+func setVelocity(new_velocity: Vector3):
+	velocity = new_velocity
+
+func setVelocityX(new_velocity_x: float):
+	velocity.x = new_velocity_x
+
+func setVelocityY(new_velocity_y: float):
+	velocity.y = new_velocity_y
+
+func setVelocityZ(new_velocity_z: float):
+	velocity.z = new_velocity_z
+
+ 
+# Getter Funcitons
+func getVelocity() -> Vector3:
+	return velocity
+
+func freeEnemy():
+	if(get_owner().global_transform.origin.z > 2):
+		PlayerStates.incrementPlayerCurrentScore()
+		get_owner().queue_free()
+
