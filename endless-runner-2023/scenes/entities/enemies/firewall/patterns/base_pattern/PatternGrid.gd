4cf3416ff09b94907d9487196c11a5f1ef509086
Michael <c21502889@mytudubllin.ie>
2023-03-01 12:06:08 +0000

    commit

---
+++ endless-runner-2023/scenes/entities/enemies/firewall/patterns/base_pattern/PatternGrid.gd
@@ -0,0 +1,17 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+const grid_height = 9
+const grid_width = 9
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
